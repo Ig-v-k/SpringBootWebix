8b5ade6e528904b797ea3aed9aca4423f82e9f75
Igor Vysotskyi <lgorvysocky@gmail.com>
2020-08-31 14:18:35 +0200

    entities

--- src/main/java/com/project/webixs/logistic/LogisticApplication.java
+++ src/main/java/com/project/webixs/logistic/LogisticApplication.java
@@ -1,7 +1,14 @@
 package com.project.webixs.logistic;
 
+import lombok.Data;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @SpringBootApplication
 public class LogisticApplication {
@@ -9,3 +16,17 @@ public class LogisticApplication {
 	SpringApplication.run(LogisticApplication.class, args);
   }
 }
+
+/*
+ * 	Entities
+ */
+@Data
+@Entity
+class Mark {
+  @Id
+  @GeneratedValue(strategy = GenerationType.TABLE)
+  private Long id;
+
+  private String name;
+}
+
