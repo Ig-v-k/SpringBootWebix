2f314cdc328f7bafed17da1ab0fad8f87ce9a61b
Igor Vysotskyi <lgorvysocky@gmail.com>
2020-08-31 15:10:40 +0200

    controller abstract

--- src/main/java/com/project/webixs/logistic/LogisticApplication.java
+++ src/main/java/com/project/webixs/logistic/LogisticApplication.java
@@ -1,9 +1,14 @@
 package com.project.webixs.logistic;
 
 import lombok.Data;
+import org.springframework.beans.BeanUtils;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.web.bind.annotation.*;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -14,6 +19,44 @@ import javax.persistence.Id;
 public class LogisticApplication {
   public static void main(String[] args) {
 	SpringApplication.run(LogisticApplication.class, args);
+  }
+}
+
+/*
+ *   Controllers
+ */
+class AbstractRestController<T, R extends JpaRepository<T, ?>> {
+  protected R repository;
+
+  public AbstractRestController(R repository) {
+    this.repository = repository;
+  }
+
+  @GetMapping
+  public Page<T> list(@PageableDefault Pageable pageable) {
+    return repository.findAll(pageable);
+  }
+
+  @GetMapping("{id}")
+  public T getOne(@PathVariable("id") T obj) {
+    return obj;
+  }
+
+  @PostMapping
+  public T add(@RequestBody T obj) {
+    return repository.save(obj);
+  }
+
+  @PutMapping("{id}")
+  public T update(@PathVariable("id") T dbObj, @RequestBody T obj) {
+    BeanUtils.copyProperties(obj, dbObj, "id");
+
+    return repository.save(dbObj);
+  }
+
+  @DeleteMapping("{id}")
+  public void delete(@PathVariable("id") T dbObj) {
+    repository.delete(dbObj);
   }
 }
 
@@ -33,4 +76,5 @@ class Mark {
 /*
  *   Repositories
  */
-interface MarkRepository extends JpaRepository<Mark, Long> {}
+interface MarkRepository extends JpaRepository<Mark, Long> {
+}
